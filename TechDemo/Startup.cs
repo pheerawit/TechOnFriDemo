e070fbc01fc45610f986dc39452d25aa1296669f
pheerawit <phee.ph@hotmail.com>
2016-07-14 15:39:01 +0700

    Initial Commit

---
+++ TechDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechDemo.Startup))]
+namespace TechDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
